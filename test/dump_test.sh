9c73ea2eacf85418e2bb417cc20f9115b5bff992
Simon Wood <simon@mungewell.org>
2019-10-28 21:27:28 -0600

    Clean up dumps

--- test/dump_test.sh
+++
@@ -1,7 +0,0 @@
-# Dump the contents of the factory, Synthmania and hAQ_aTTAQ packs...
-
-find 'Factory 21-100'/ -name '*.unosyp' -exec bash -c "echo '{}';python ../uno_synth.py -d '{}'" \; >> factory_dump.txt
-find Synthmania/ -name '*.unosyp' -exec bash -c "echo '{}';python ../uno_synth.py -d '{}'" \; >> synthmania_dump.txt
-find '80_UNO_Synth_ presets_by_haQ_attaQ'/ -name '*.unosyp' -exec bash -c "echo '{}';python ../uno_synth.py -d '{}'" \; >> haq_attaq_dump.txt
-find 'UNO_Synth_Presets_By_Blandy'/ -name '*.unosyp' -exec bash -c "echo '{}';python ../uno_synth.py -d '{}'" \; >> blandy_dump.txt
-find 'ADSR Urban'/ -name '*.unosyp' -exec bash -c "echo '{}';python ../uno_synth.py -d '{}'" \; >> adsr_urban.txt
