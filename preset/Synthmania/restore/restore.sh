92b6fdbcbec7d3b2765f601b3b3ec773fc9c3818
Simon Wood <simon@mungewell.org>
2019-11-16 11:36:58 -0700

    Remove 'unknown' patch

--- preset/Synthmania/restore/restore.sh
+++ preset/Synthmania/restore/restore.sh
@@ -30,51 +30,51 @@ ln -s ../"Downpour.unosyp"                                             49.unosyp
 ln -s ../"Early Rave.unosyp"                                           50.unosyp
 ln -s ../"Electro Arpeggio.unosyp"                                     51.unosyp
 ln -s ../"Electro Sequence.unosyp"                                     52.unosyp
-ln -s ../"Electro Sequence_unknown7.unosyp"                            53.unosyp
-ln -s ../"Elvis Lead.unosyp"                                           54.unosyp
-ln -s ../"Euro Ambulance.unosyp"                                       55.unosyp
-ln -s ../"Faun Flute.unosyp"                                           56.unosyp
-ln -s ../"Fishing Boat.unosyp"                                         57.unosyp
-ln -s ../"Funk Master.unosyp"                                          58.unosyp
-ln -s ../"Glacier Diva.unosyp"                                         59.unosyp
-ln -s ../"Hardcore Uproar.unosyp"                                      60.unosyp
-ln -s ../"I Like To Moov It.unosyp"                                    61.unosyp
-ln -s ../"Ice Cream Truck.unosyp"                                      62.unosyp
-ln -s ../"Industrial Bass.unosyp"                                      63.unosyp
-ln -s ../"Inside The CPU.unosyp"                                       64.unosyp
-ln -s ../"Ivory Lead.unosyp"                                           65.unosyp
-ln -s ../"Laser Cannons.unosyp"                                        66.unosyp
-ln -s ../"London Sequence.unosyp"                                      67.unosyp
-ln -s ../"Lucky Squares.unosyp"                                        68.unosyp
-ln -s ../"Lunar Base.unosyp"                                           69.unosyp
-ln -s ../"Mirrored Rave.unosyp"                                        70.unosyp
-ln -s ../"Modular Lead.unosyp"                                         71.unosyp
-ln -s ../"Morse Transmission.unosyp"                                   72.unosyp
-ln -s ../"Octopus.unosyp"                                              73.unosyp
-ln -s ../"PWM King.unosyp"                                             74.unosyp
-ln -s ../"Paft Dunker.unosyp"                                          75.unosyp
-ln -s ../"Persuaders Bass.unosyp"                                      76.unosyp
-ln -s ../"Planet Soul.unosyp"                                          77.unosyp
-ln -s ../"Popcorn Lead.unosyp"                                         78.unosyp
-ln -s ../"Pulsing Bass.unosyp"                                         79.unosyp
-ln -s ../"Rain & Wind.unosyp"                                          80.unosyp
-ln -s ../"Rave Shield.unosyp"                                          81.unosyp
-ln -s ../"Retro Sequence.unosyp"                                       82.unosyp
-ln -s ../"Sci-Fi Drone.unosyp"                                         83.unosyp
-ln -s ../"See You Lately.unosyp"                                       84.unosyp
-ln -s ../"Snap Seq.unosyp"                                             85.unosyp
-ln -s ../"Space Gulls.unosyp"                                          86.unosyp
-ln -s ../"Space Puppies.unosyp"                                        87.unosyp
-ln -s ../"Sprite Voice.unosyp"                                         88.unosyp
-ln -s ../"Synth Pop Seq.unosyp"                                        89.unosyp
-ln -s ../"Talk 2 Much.unosyp"                                          90.unosyp
-ln -s ../"Telstarry Eyes.unosyp"                                       91.unosyp
-ln -s ../"Thru The Night.unosyp"                                       92.unosyp
-ln -s ../"Thunder Bass.unosyp"                                         93.unosyp
-ln -s ../"Under The Skin.unosyp"                                       94.unosyp
-ln -s ../"VL Piano.unosyp"                                             95.unosyp
-ln -s ../"Vampire Lead.unosyp"                                         96.unosyp
-ln -s ../"Venga Lead.unosyp"                                           97.unosyp
-ln -s ../"Warm Bass.unosyp"                                            98.unosyp
-ln -s ../"Wind Lead.unosyp"                                            99.unosyp
-ln -s ../"Without Limits.unosyp"                                       100.unosyp
+ln -s ../"Elvis Lead.unosyp"                                           53.unosyp
+ln -s ../"Euro Ambulance.unosyp"                                       54.unosyp
+ln -s ../"Faun Flute.unosyp"                                           55.unosyp
+ln -s ../"Fishing Boat.unosyp"                                         56.unosyp
+ln -s ../"Funk Master.unosyp"                                          57.unosyp
+ln -s ../"Glacier Diva.unosyp"                                         58.unosyp
+ln -s ../"Hardcore Uproar.unosyp"                                      59.unosyp
+ln -s ../"I Like To Moov It.unosyp"                                    60.unosyp
+ln -s ../"Ice Cream Truck.unosyp"                                      61.unosyp
+ln -s ../"Industrial Bass.unosyp"                                      62.unosyp
+ln -s ../"Inside The CPU.unosyp"                                       63.unosyp
+ln -s ../"Ivory Lead.unosyp"                                           64.unosyp
+ln -s ../"Laser Cannons.unosyp"                                        65.unosyp
+ln -s ../"London Sequence.unosyp"                                      66.unosyp
+ln -s ../"Lucky Squares.unosyp"                                        67.unosyp
+ln -s ../"Lunar Base.unosyp"                                           68.unosyp
+ln -s ../"Mirrored Rave.unosyp"                                        69.unosyp
+ln -s ../"Modular Lead.unosyp"                                         70.unosyp
+ln -s ../"Morse Transmission.unosyp"                                   71.unosyp
+ln -s ../"Octopus.unosyp"                                              72.unosyp
+ln -s ../"PWM King.unosyp"                                             73.unosyp
+ln -s ../"Paft Dunker.unosyp"                                          74.unosyp
+ln -s ../"Persuaders Bass.unosyp"                                      75.unosyp
+ln -s ../"Planet Soul.unosyp"                                          76.unosyp
+ln -s ../"Popcorn Lead.unosyp"                                         77.unosyp
+ln -s ../"Pulsing Bass.unosyp"                                         78.unosyp
+ln -s ../"Rain & Wind.unosyp"                                          79.unosyp
+ln -s ../"Rave Shield.unosyp"                                          80.unosyp
+ln -s ../"Retro Sequence.unosyp"                                       81.unosyp
+ln -s ../"Sci-Fi Drone.unosyp"                                         82.unosyp
+ln -s ../"See You Lately.unosyp"                                       83.unosyp
+ln -s ../"Snap Seq.unosyp"                                             84.unosyp
+ln -s ../"Space Gulls.unosyp"                                          85.unosyp
+ln -s ../"Space Puppies.unosyp"                                        86.unosyp
+ln -s ../"Sprite Voice.unosyp"                                         87.unosyp
+ln -s ../"Synth Pop Seq.unosyp"                                        88.unosyp
+ln -s ../"Talk 2 Much.unosyp"                                          89.unosyp
+ln -s ../"Telstarry Eyes.unosyp"                                       90.unosyp
+ln -s ../"Thru The Night.unosyp"                                       91.unosyp
+ln -s ../"Thunder Bass.unosyp"                                         92.unosyp
+ln -s ../"Under The Skin.unosyp"                                       93.unosyp
+ln -s ../"VL Piano.unosyp"                                             94.unosyp
+ln -s ../"Vampire Lead.unosyp"                                         95.unosyp
+ln -s ../"Venga Lead.unosyp"                                           96.unosyp
+ln -s ../"Warm Bass.unosyp"                                            97.unosyp
+ln -s ../"Wind Lead.unosyp"                                            98.unosyp
+ln -s ../"Without Limits.unosyp"                                       99.unosyp
+ln -s ../"Yellow Puppet.unosyp"                                        100.unosyp
