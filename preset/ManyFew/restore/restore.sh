9cc7e28d81620d0cfe8647e69f39c3411bc5b614
Simon Wood <simon@mungewell.org>
2023-03-04 16:34:38 -0700

    ManyFew preset

---
+++ preset/ManyFew/restore/restore.sh
@@ -0,0 +1,39 @@
+ln -s ../"Bass Brazil.unosyp"                                          21.unosyp
+ln -s ../"Bass Dark.unosyp"                                            22.unosyp
+ln -s ../"Bass Deep LFO.unosyp"                                        23.unosyp
+ln -s ../"Bass Delay.unosyp"                                           24.unosyp
+ln -s ../"Bass Disco.unosyp"                                           25.unosyp
+ln -s ../"Bass Fat Analog.unosyp"                                      26.unosyp
+ln -s ../"Bass Favorite.unosyp"                                        27.unosyp
+ln -s ../"Bass Filtered Saw.unosyp"                                    28.unosyp
+ln -s ../"Bass Hello.unosyp"                                           29.unosyp
+ln -s ../"Bass HipHop.unosyp"                                          30.unosyp
+ln -s ../"Bass Hold Down.unosyp"                                       31.unosyp
+ln -s ../"Bass Jump.unosyp"                                            32.unosyp
+ln -s ../"Bass Rich.unosyp"                                            33.unosyp
+ln -s ../"Bass Saw.unosyp"                                             34.unosyp
+ln -s ../"Bass Sub.unosyp"                                             35.unosyp
+ln -s ../"Bass Super Deep.unosyp"                                      36.unosyp
+ln -s ../"FX Boom.unosyp"                                              37.unosyp
+ln -s ../"FX Hihat.unosyp"                                             38.unosyp
+ln -s ../"FX LFO Noise.unosyp"                                         39.unosyp
+ln -s ../"FX Percussion.unosyp"                                        40.unosyp
+ln -s ../"FX Rain Sweep.unosyp"                                        41.unosyp
+ln -s ../"FX Snare:Clap Noise.unosyp"                                  42.unosyp
+ln -s ../"FX Sweep.unosyp"                                             43.unosyp
+ln -s ../"FX Wind.unosyp"                                              44.unosyp
+ln -s ../"Seq City.unosyp"                                             45.unosyp
+ln -s ../"Seq Da Funk.unosyp"                                          46.unosyp
+ln -s ../"Seq Filtered Freedom.unosyp"                                 47.unosyp
+ln -s ../"Seq Groove.unosyp"                                           48.unosyp
+ln -s ../"Seq Lab.unosyp"                                              49.unosyp
+ln -s ../"Seq Rave.unosyp"                                             50.unosyp
+ln -s ../"Seq Techno.unosyp"                                           51.unosyp
+ln -s ../"Seq Turtles.unosyp"                                          52.unosyp
+ln -s ../"Seq Video Games.unosyp"                                      53.unosyp
+ln -s ../"Synth Bathroom.unosyp"                                       54.unosyp
+ln -s ../"Synth French.unosyp"                                         55.unosyp
+ln -s ../"Synth Glide.unosyp"                                          56.unosyp
+ln -s ../"Synth Keys.unosyp"                                           57.unosyp
+ln -s ../"Synth Light.unosyp"                                          58.unosyp
+ln -s ../"Synth Play Me.unosyp"                                        59.unosyp
